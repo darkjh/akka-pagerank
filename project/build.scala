5d35f5cd68f5c6e91d774c74f4d5a0f664b379f6
Han JU <ju.han.felix@gmail.com>
2014-10-28 01:06:31 +0100

    project setup

---
+++ project/build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+
+object AkkaPageRankBuild extends Build {
+
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    // project settings
+    version := "0.1.0-SNAPSHOT",
+    organization := "me.juhanlol",
+    scalaVersion := "2.10.4",
+    // dependencies
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "1.9.2" % "test",
+      "com.typesafe.akka" %% "akka-actor" % "2.3.6")
+  )
+
+  lazy val project = Project("akka-pagerank", file("."),
+    settings = buildSettings)
+}
