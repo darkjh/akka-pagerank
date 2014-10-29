21272905cfd8f6f4ad3bfcc9647ec300bfa9fab5
Han JU <ju.han.felix@gmail.com>
2014-10-29 18:52:17 +0100

    add cassovary as dependency

--- project/build.scala
+++ project/build.scala
@@ -12,8 +12,14 @@ object AkkaPageRankBuild extends Build {
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "2.2.1" % "test",
       "com.typesafe.akka" %% "akka-actor" % "2.3.6",
+
       "org.scalanlp" %% "breeze" % "0.10",
-      "org.scalanlp" %% "breeze-natives" % "0.10"
+      "org.scalanlp" %% "breeze-natives" % "0.10",
+
+      // need to locally build and publish Cassovary
+      // it's not on maven central
+      "com.twitter" %% "cassovary-core" % "3.3.1",
+      "it.unimi.dsi" % "fastutil" % "6.4.4"
     )
   )
 
