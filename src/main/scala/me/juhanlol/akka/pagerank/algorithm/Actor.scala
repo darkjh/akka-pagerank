d9c3d06a08099722ed333aded5ff550a796ddab5
Han JU <ju.han.felix@gmail.com>
2014-11-07 01:47:57 +0100

    master work partition

--- src/main/scala/me/juhanlol/akka/pagerank/algorithm/Actor.scala
+++ src/main/scala/me/juhanlol/akka/pagerank/algorithm/Actor.scala
@@ -2,15 +2,50 @@ package me.juhanlol.akka.pagerank.algorithm
 
 import me.juhanlol.akka.pagerank.Graph
 import akka.actor._
+import breeze.linalg._
+import akka.routing.RoundRobinRouter
 
 
 sealed trait PRMessage
 
 case object Compute extends PRMessage
 
-case class Work(modulo: Int) extends PRMessage
+case class Work(start: Int, size: Int, graph: Graph) extends PRMessage
 
-case class Result(value: Double) extends PRMessage
+case class PartialResult(start: Int, r: DenseVector[Double]) extends PRMessage
+
+
+class Worker extends Actor {
+  def partialUpdate() = {
+
+  }
+
+  override def receive: Actor.Receive = {
+    case Work(start, size, g) =>
+  }
+}
+
+class Master(g: Graph, partitions: Int) extends Actor {
+  val workerRouter = context.actorOf(
+      Props[Worker].withRouter(RoundRobinRouter(partitions)),
+    name = "workerRouter")
+
+  val size = g.nodeCount / partitions
+  var starts = 0 until g.nodeCount by size
+
+  override def receive: Actor.Receive = {
+    case Compute =>
+      for (start <- starts) {
+        val s = if (g.nodeCount - start < size) {
+          // last one
+          g.nodeCount - start
+        } else {
+          size
+        }
+        workerRouter ! Work(start, size, g)
+      }
+  }
+}
 
 
 class ActorPageRankAlgorithm(params: PageRankParams)(implicit system: ActorSystem)
